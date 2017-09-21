408066dd586ceb1e874ca386870b246ac7f98cd5
Akshit Kumar <akshitkumar100@gmail.com>
2017-09-21 13:13:12 +0530

    Fix Bud

--- Assignment-2/code/optimize.py
+++ Assignment-2/code/optimize.py
@@ -20,16 +20,15 @@ lambda2 = (num_items - x) / num_items
 ES1 = (x + 1)/(2*r)
 ES2 = (x + 41)/(2*r)
 ES1sq = np.multiply((x+1),(2*x + 1))/(6*r*r)
-ES2sq = np.divide((40.0*41.0*81.0 - np.multiply(x,np.multiply(x+1,2*x+1))),6*(40-x)*r)
+ES2sq = np.divide((40.0*41.0*81.0 - np.multiply(x,np.multiply(x+1,2*x+1))),6*(40-x)*r*r)
 rho1 = np.multiply(lambda1,ES1)
 rho2 = np.multiply(lambda2,ES2)
-W1 = np.divide(np.multiply(lambda1,ES1),2*(1 - rho1))
-W2 = np.divide(np.multiply(lambda2,ES2),2*(1 - rho2))
+W1 = np.divide(np.multiply(lambda1,ES1sq),2*(1 - rho1))
+W2 = np.divide(np.multiply(lambda2,ES2sq),2*(1 - rho2))
 W = np.multiply(lambda1,W1) + np.multiply(lambda2,W2)
 
 # Displaying the necessary information
 print "Minimum value is achieved at %d" %(x[np.argmin(W)])
-print W
 plt.plot(x,W,'bo')
 plt.xlabel(r'$x$')
 plt.ylabel(r'$W(x)$')
