6467ee8f645add434a79a84c871516a5ef2d0cb2
AkshitKumar <akshitkumar100@gmail.com>
2017-10-02 10:48:28 +0530

    Make functions

--- Assignment-3/simulation.cpp
+++ Assignment-3/simulation.cpp
@@ -31,34 +31,39 @@ float getTime(string time){
 	return strtof(time.c_str(),NULL);
 }
 
+Packet packetDetails(string str,string delimiter){
+	size_t pos = 0;
+	string token;
+	int item = 0;
+	Packet p;
+	while((pos = str.find(delimiter)) != string::npos){
+		token = str.substr(0,pos);
+		switch(item){
+			case 0: 
+				p.id = getId(token);
+				break;
+			case 1:
+				p.time = getTime(token);
+				break;
+			case 2 :
+				p.size = getSize(token);
+				break;
+			default :
+				cout << "Error" << endl;
+		}
+		item += 1;
+		str.erase(0,pos + delimiter.length());
+	}
+	p.queueId = getQueueId(str);
+	return p;
+}
+
 int main(){
 	ifstream file("input.txt");
 	string str;
 	string delimiter = ";";
 	while(getline(file,str)){
-		size_t pos = 0;
-		string token;
-		int item = 0;
-		Packet p;
-		while((pos = str.find(delimiter)) != string::npos){
-			token = str.substr(0,pos);
-			switch(item){
-				case 0: 
-					p.id = getId(token);
-					break;
-				case 1:
-					p.time = getTime(token);
-					break;
-				case 2 :
-					p.size = getSize(token);
-					break;
-				default :
-					cout << "Error" << endl;
-			}
-			item += 1;
-			str.erase(0,pos + delimiter.length());
-		}
-		p.queueId = getQueueId(str);
+		Packet p = packetDetails(str,delimiter);
 		cout << p.id << " " << p.time << " " << p.size << " " << p.queueId << endl;
 	}
 	return 0;
